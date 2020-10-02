cd8e96112421667a5590e2831315c2e861b099ac
Sherry Chen <sherrychen127@gmail.com>
2020-10-02 13:44:20 +0800

    added lab3 race track/launch file/starter code

--- nodes/lab01_motor.py
+++ nodes/lab01_motor.py
@@ -10,8 +10,34 @@ def publisher_node():
     '''
     TODO: complete the publisher function here
     '''
-    pass
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    rate = rospy.Rate(10)
+    
+    forward_movement = Twist()
+    forward_movement.linear.x = 0.1
+    spin_movement = Twist()
+    spin_movement.angular.z = math.pi/2
+    stop_movement = Twist()
+    stop_movement.linear.x = 0
+    stop_movement.linear.y = 0
+    stop_movement.linear.z = 0
+    stop_movement.angular.x = 0
+    stop_movement.angular.y = 0
+    stop_movement.angular.z = 0
 
+    counter = 0
+
+    while not rospy.is_shutdown():
+        print(counter)
+        counter += 1
+        if counter < 101:
+            pub.publish(forward_movement)
+        elif counter >= 101 and counter < 141:
+            pub.publish(spin_movement)
+        else:
+            pub.publish(stop_movement)
+
+        rate.sleep()
 
 def main():
 
--- nodes/lab01_odom.py
+++ nodes/lab01_odom.py
@@ -12,15 +12,20 @@ def get_yaw_from_quarternion(q):
 
 def callback(odom_data):
 	'''TODO: complete the call back function for subscriber'''
-
-	pass
+	point = odom_data.pose.pose.position 
+	quart = odom_data.pose.pose.orientation 
+	theta=get_yaw_from_quarternion(quart)
+	cur_pose = (point.x, point.y, theta) 
+	rospy.loginfo(cur_pose)
 
 
 def main():
 	try:
 		rospy.init_node('odometry')
 		'''TODO: initialize the subscriber of odometery here'''
-
+		rate = rospy.Rate(10) 
+		listen = rospy.Subscriber('odom', Odometry, callback, queue_size=1)
+		rate.sleep()
 		rospy.spin()
 	except rospy.ROSInterruptException:
 		pass
