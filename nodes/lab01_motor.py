bea6a9d1757bf7c7c4c797d03a9d2ea24ce47c4f
sherrychen127 <sherrychen127@gmail.com>
2020-10-02 13:56:31 +0800

    reset

--- nodes/lab01_motor.py
+++ nodes/lab01_motor.py
@@ -10,34 +10,8 @@ def publisher_node():
     '''
     TODO: complete the publisher function here
     '''
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-    rate = rospy.Rate(10)
-    
-    forward_movement = Twist()
-    forward_movement.linear.x = 0.1
-    spin_movement = Twist()
-    spin_movement.angular.z = math.pi/2
-    stop_movement = Twist()
-    stop_movement.linear.x = 0
-    stop_movement.linear.y = 0
-    stop_movement.linear.z = 0
-    stop_movement.angular.x = 0
-    stop_movement.angular.y = 0
-    stop_movement.angular.z = 0
+    pass
 
-    counter = 0
-
-    while not rospy.is_shutdown():
-        print(counter)
-        counter += 1
-        if counter < 101:
-            pub.publish(forward_movement)
-        elif counter >= 101 and counter < 141:
-            pub.publish(spin_movement)
-        else:
-            pub.publish(stop_movement)
-
-        rate.sleep()
 
 def main():
 
