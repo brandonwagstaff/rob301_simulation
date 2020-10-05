24b6a7cffbc3750a0ecf495953c58acc96b401e9
Sherry Chen <sherrychen127@gmail.com>
2020-10-05 13:06:02 -0400

    Revert "Revert "reset""
    This reverts commit 5b49a9bebde95ba884daf58903edd0418c631b8e.

--- nodes/lab01_odom.py
+++ nodes/lab01_odom.py
@@ -12,20 +12,15 @@ def get_yaw_from_quarternion(q):
 
 def callback(odom_data):
 	'''TODO: complete the call back function for subscriber'''
-	point = odom_data.pose.pose.position 
-	quart = odom_data.pose.pose.orientation 
-	theta=get_yaw_from_quarternion(quart)
-	cur_pose = (point.x, point.y, theta) 
-	rospy.loginfo(cur_pose)
+
+	pass
 
 
 def main():
 	try:
 		rospy.init_node('odometry')
 		'''TODO: initialize the subscriber of odometery here'''
-		rate = rospy.Rate(10) 
-		listen = rospy.Subscriber('odom', Odometry, callback, queue_size=1)
-		rate.sleep()
+
 		rospy.spin()
 	except rospy.ROSInterruptException:
 		pass
